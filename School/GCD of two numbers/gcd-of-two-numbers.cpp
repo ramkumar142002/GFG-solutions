ffe994523f017d44a016f92eac84bef3d2a2ce4b
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2024-03-15 01:39:10 +0530

    Added Solution - GfG to GitHub

---
+++ School/GCD of two numbers/gcd-of-two-numbers.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int gcd(int a, int b) {
+        // code here
+        if(a == 0) return b;
+        return gcd(b%a,a);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int a;
+        scanf("%d",&a);
+        
+        
+        int b;
+        scanf("%d",&b);
+        
+        Solution obj;
+        int res = obj.gcd(a, b);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
