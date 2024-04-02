b2af724da542cf0a847c631e08b0279e96fc475b
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2024-04-02 17:15:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find all pairs with a given sum/find-all-pairs-with-a-given-sum.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
+    {
+        // Your code goes here   
+        sort(A,A+N);
+        sort(B,B+M);
+        vector<pair<int,int>> ans;
+        
+        // for(int i = 0;i<N;i++) cout<<A[i]<< " ";
+        // cout<<endl;
+        // for(int i = 0;i<M;i++) cout<<B[i]<< " ";
+        // cout<<endl;
+        
+        for(int i = 0;i<N;i++){
+            int j = 0;
+            int k = M-1;
+            while(j<=k){
+                
+                int mid = j + (k-j)/2;
+                // cout<<A[i]<<" "<<B[mid]<<endl;
+                if(A[i] + B[mid] == X){
+                    ans.push_back(make_pair(A[i],B[mid]));
+                    break;
+                }
+                else if(A[i] + B[mid] > X){
+                    k = mid - 1;
+                }
+                else j = mid + 1;
+            }
+        }
+        return ans;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    int N, M, X;
+	    cin >> N >> M >> X;
+        int A[N], B[M];
+        
+	    for(int i = 0;i<N;i++)
+	        cin >> A[i];
+	    for(int i = 0;i<M;i++)
+	        cin >> B[i];
+	        
+	   Solution ob;
+	   vector<pair<int,int>> vp = ob.allPairs(A, B, N, M, X);
+	   int sz = vp.size();
+        if(sz==0)
+        cout<<-1<<endl;
+        else{
+            for(int i=0;i<sz;i++){
+                if(i==0)
+                cout<<vp[i].first<<" "<<vp[i].second;
+                else
+                cout<<", "<<vp[i].first<<" "<<vp[i].second;
+            }
+            cout<<endl;
+        }
+	}
+	return 0;
+}
+// } Driver Code Ends
