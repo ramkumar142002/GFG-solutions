35945660349e740483b9e3552d5c6e834dba6d93
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2024-06-26 00:42:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Left Rotate Matrix K times/left-rotate-matrix-k-times.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> rotateMatrix(int k, vector<vector<int>> matrix) {
+        // code here
+        k = k % matrix[0].size();
+        
+        for(int i = 0;i<matrix.size();i++){
+            reverse(matrix[i].begin(),matrix[i].begin()+k);
+            reverse(matrix[i].begin()+k,matrix[i].end());
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+        
+        return matrix;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, k;
+        cin >> n >> m >> k;
+        vector<vector<int>> mat(n, vector<int>(m));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> mat[i][j];
+        Solution ob;
+        vector<vector<int>> ans = ob.rotateMatrix(k, mat);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                cout << ans[i][j] << " ";
+            cout << "\n";
+        }
+    }
+}
+
+// } Driver Code Ends
