95ff8947b9938b560d7a2a8def9167a1699d531a
Ramkumar SV <95877257+ramkumar142002@users.noreply.github.com>
2025-01-13 23:24:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Sorting {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            System.out.println(new Solution().maxWater(arr));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    public int maxWater(int arr[]) {
+        // Code Here
+        int i = 0, j = arr.length-1;
+        int maxi = 0;
+        while(i<j){
+            maxi = Math.max(maxi, (j-i)*Math.min(arr[j],arr[i]));
+            if(arr[j]>arr[i]){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return maxi;
+    }
+}
